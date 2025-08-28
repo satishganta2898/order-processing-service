65f60703b685ec099eb2f0a1ac961c1691829173
Satish Ganta <163132275+satishganta2898@users.noreply.github.com>
2025-08-28 21:22:41 +0530

    Added Order and Product JPA repositories

---
+++ src/main/java/com/quicken/order/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.quicken.order.repository;
+
+import com.quicken.order.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
