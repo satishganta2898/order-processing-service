488abb485176bec6bf3213b0ada08e759b47ef0d
Satish Ganta <163132275+satishganta2898@users.noreply.github.com>
2025-08-28 22:43:58 +0530

    Implemented Exception Handling and DB connection
    Added @EnableAsync for asynchronous processing

---
+++ src/main/java/com/quicken/order/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.quicken.order.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+}
